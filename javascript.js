113ccaf893b1a35d4d7831b486eabb7bdd9521eb
SaraToth <saratoth101@gmail.com>
2024-04-14 14:51:13 +0900

    Create runOperation function that takes two numbers and decides which mathematical function to run and return

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let firstNumber;
+let secondNumber;
+
 function add(a, b) {
     return a + b;
 };
@@ -13,3 +16,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function runOperation(firstNumber, secondNumber, operator) {
+    if(operator === "add") {
+        return add(firstNumber, secondNumber);
+    } else if(operator === "multiply") {
+        return multiply(firstNumber, secondNumber);
+    } else if(operator === "divide") {
+        return divide(firstNumber, secondNumber);
+    } else if(operator === "subtract") {
+        return subtract(firstNumber, secondNumber);
+    };
+};
+
